ff7526e0020aba6696ad9cf9444e11f1d81420ea
JoseLandivar <joseantoniolandivar@gmail.com>
2021-09-20 09:53:43 -0400

    Dia-1

---
+++ Javascript/01.Variables.js
@@ -0,0 +1,42 @@
+// var, let y const
+// variabel es guardar un espacio en memoria
+
+//Declaracion de la variable
+let mascota = null;
+
+//Inicializacion de la variable
+mascota = 'Perro'
+
+//Declaracion e inicializacion de la variable
+let mascota2 = "Perro"
+
+console.log(mascota)
+
+/*Reglas
+1º No se puedo declarar dos variable con el mism nombre
+2º let permite declara la variable y no inicializarla
+3º var me permite declara otra variable con el mismo nombre
+4º var permite declara sin inicializar
+5º const no nos deja declarar una variable sin inicializarla
+
+*/
+
+var nombre = "Jose"
+console.log(nombre)
+
+var nombre = "Juan"
+console.log(nombre)
+
+//variable undefined o variable null
+
+// let pez;
+// console.log(pez)
+
+
+let edad = null
+console.log(edad)
+
+//const
+
+const perro = "Oddie";
+console.log(perro)
